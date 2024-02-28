35c782faded0e5d85aaace97cbbe07b1e35696c3
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-02-28 15:08:38 +0200

    djikstra network

--- CodeCPP/Dijkstra.cpp
+++ CodeCPP/Dijkstra.cpp
@@ -2,76 +2,97 @@
 #include <vector>
 
 using namespace std;
-// _____________________
-//|                     |
-//| Dijkstra algorithm  |
-//| implemented in C++  |
-//|_____________________|
 
-// _____________________
-//|                     |
-//|        NODE         |
-//|_____________________|
 class Node
 {
 public:
-  Node(int id);
-
-private:
   int id;
-  vector<pair<Node *, int>> connections;
+  vector<pair<int, Node *>> connectedNodes; // List of value and pointer pair
 
-}; // Class Node
+  Node(int id) : id(id) {} // Constructor
 
-Node::Node(int id)
-{
-  this->id = id;
-} // Node
-// _____________________
-//|                     |
-//|        NODE         |
-//|_____________________|
-
-// _____________________
-//|                     |
-//|       NETWORK       |
-//|_____________________|
-class Network
-{
-
-public:
-  Network(/* args */);
-  ~Network();
-
-  void addNode();
-
-private:
-  /* data */
+  // Add a function to connect this node to another node
+  void connect(Node *other, int value)
+  {
+    connectedNodes.push_back(make_pair(value, other));
+  }
 };
 
-Network::Network(/* args */)
+class Network
 {
+public:
+  Network();
+  ~Network();
+
+  void addNode(int id, int value);
+
+  int nodes;
+  Node *root;
+};
+
+Network::Network()
+{
+  nodes = 0;      // Initialize to 0, we increment it when adding nodes
+  root = nullptr; // Initialize to nullptr
 }
 
 Network::~Network()
 {
+  // Implement destructor if necessary
 }
 
-// _____________________
-//|                     |
-//|       NETWORK       |
-//|_____________________|
+void Network::addNode(int id, int value)
+{
+  // Create a new node
+  Node *newNode = new Node(id);
 
-// _____________________
-//|                     |
-//|        MAIN         |
-//|_____________________|
+  if (!root)
+  {
+    root = newNode; // If root is nullptr, set the new node as root
+  }
+  else
+  {
+    // Connect the new node to the previous node
+    Node *prevNode = root;
+    while (!prevNode->connectedNodes.empty())
+    {
+      prevNode = prevNode->connectedNodes[0]; // Follow the chain of connections
+    }
+    prevNode->connect(newNode);
+  }
+
+  nodes++; // Increment the node count
+}
+
 int main()
 {
+  // Create a network
+  Network network;
+
+  // Add nodes
+  network.addNode(0, 0);
+  network.addNode(1, 5);
+  network.addNode(2, 8);
+
+  // Print connections (just for visualization)
+  Node *current = network.root;
+  while (current)
+  {
+    cout << "Node " << current->id << " is connected to: ";
+    for (Node *connected : current->connectedNodes)
+    {
+      cout << connected->id << " ";
+    }
+    cout << endl;
+    if (!current->connectedNodes.empty())
+    {
+      current = current->connectedNodes[0];
+    }
+    else
+    {
+      current = nullptr;
+    }
+  }
 
   return 0;
 }
-// _____________________
-//|                     |
-//|        MAIN         |
-//|_____________________|
