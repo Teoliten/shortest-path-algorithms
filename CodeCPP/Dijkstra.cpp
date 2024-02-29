323913277ea9a0dd53784e905bd81f7ad7238564
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-02-29 12:35:24 +0200

    backup

--- CodeCPP/Dijkstra.cpp
+++ CodeCPP/Dijkstra.cpp
@@ -4,26 +4,24 @@
 using namespace std;
 
 // ------------ NODE ------------
-
 class Node
 {
 public:
   Node(int id) : id(id) {} // Constructor
-
-  // Add a function to connect this node to another node
   void connect(int value, Node *other);
-  // friend class Network;
   int getId() const { return id; }
 
 private:
   int id;
-  vector<pair<int, Node *>> connectedNodes; // List of value and pointer pair
-};
+  vector<pair<int, Node *>> connectedNodes; // connection value, and pointer to node
+
+}; // Class Node
 
 void Node::connect(int value, Node *other)
 {
   connectedNodes.push_back(make_pair(value, other));
 } // connect
+
 // ------------ NODE ------------
 
 // ------------ NETWORK ------------
@@ -34,12 +32,13 @@ public:
   ~Network();
 
   void addNode(int id, int value);
+  void addConnection(int id, int id2);
 
 private:
   vector<pair<int, Node *>> nodesInNetwork; // id, pointer to node
   int nodes;
   Node *root;
-};
+}; // Class Network
 
 Network::Network()
 {
@@ -47,16 +46,17 @@ Network::Network()
   nodesInNetwork.push_back(make_pair(0, rootNode));
   nodes = 1;
   root = rootNode;
-}
+} // Constructor
 
 Network::~Network()
 {
   // Implement destructor if necessary
-}
+} // Destructor
 
-void Network::addNode(int id, int value) // id to connect to, value of the connection
+void Network::addNode(int id, int value)
 {
   // Create a new node
+  bool found = false;
   Node *newNode = new Node(nodes);
 
   // Search for a node with the given ID in nodesInNetwork
@@ -64,36 +64,82 @@ void Network::addNode(int id, int value) // id to connect to, value of the conne
   {
     if (pair.first == id)
     {
-      cout << "found" << endl;
+      found = true;
       // Connect the newly created node to the found node
-      pair.second->connect(id, newNode);
+      newNode->connect(value, pair.second);
+      pair.second->connect(value, newNode);
       break; // Exit loop since we found the node
-    }
-    else
-    {
-      cout << "not found" << endl;
     }
   }
 
-  // Increment the node count
-  nodes++;
+  if (found)
+  {
+    // Increment the node count
+    nodes++;
 
-  // Add the new node to the nodesInNetwork vector
-  nodesInNetwork.push_back(make_pair(nodes - 1, newNode));
-}
+    // Add the new node to the nodesInNetwork vector
+    nodesInNetwork.push_back(make_pair(nodes - 1, newNode));
+  }
+  else
+  {
+    cout << "addNode_err: connecting node not found" << endl;
+  }
+} // addNode
 
+void Network::addConnection(int id, int id2)
+{
+  Node *node1;
+  Node *node2;
+
+  bool f1 = false, f2 = false;
+
+  for (auto &pair : nodesInNetwork)
+  {
+    if (pair.first == id)
+    {
+      node1 = pair.second; // link node1 to the found node
+      f1 = true;
+    }
+    else if (pair.first == id2)
+    {
+      node2 = pair.second; // link node2 to the found node
+      f2 = true;
+    }
+  }
+
+  if (f1 && f2) // both nodes found and pointers saved
+  {
+    node1->connect(id2, node2);
+    node2->connect(id, node1);
+  }
+  else // didnt find both nodes
+  {
+    cout << "addConnection_err: nodes not found" << endl;
+  }
+} // addConnection
 // ------------ NETWORK ------------
 
 // ------------ MAIN ------------
 int main()
 {
   // Create a network
-  Network network;
+  Network network; // creates root node, id: 0
 
   // Add nodes
-  network.addNode(0, 420);
-  // network.addNode(1, 421);
-  // network.addNode(2, 422);
+  // network.addNode(0, 420);  // id: 1
+  // network.addNode(0, 421);  // id: 2
+  // network.addNode(1, 422);  // id: 3
+  // network.addNode(3, 111);  // id: 4
+  // network.addNode(10, 111); // no node with id 10, wont create node.
+
+  network.addNode(0, 2); // id:1
+  network.addNode(0, 6); // id:2
+  network.addNode(1, 5); // id:3
+  // network.addConnection(3, 2); // connecting node 2 and 3
+
+  while (true)
+  {
+  }
 
   return 0;
 } // main
