fe0bc15d633e8da09ac823ea582ba5736f8ecd0a
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-02-29 12:57:51 +0200

    network implementation finished

--- CodeCPP/Dijkstra.cpp
+++ CodeCPP/Dijkstra.cpp
@@ -7,15 +7,21 @@ using namespace std;
 class Node
 {
 public:
-  Node(int id) : id(id) {} // Constructor
+  Node(int id);
   void connect(int value, Node *other);
   int getId() const { return id; }
+
+  const vector<pair<int, Node *>> &getConnectedNodes() const { return connectedNodes; }
 
 private:
   int id;
   vector<pair<int, Node *>> connectedNodes; // connection value, and pointer to node
+};                                          // Class Node
 
-}; // Class Node
+Node::Node(int id)
+{
+  this->id = id;
+} // parameter constructor
 
 void Node::connect(int value, Node *other)
 {
@@ -32,7 +38,8 @@ public:
   ~Network();
 
   void addNode(int id, int value);
-  void addConnection(int id, int id2);
+  void addConnection(int id, int id2, int value);
+  void traverseNetwork();
 
 private:
   vector<pair<int, Node *>> nodesInNetwork; // id, pointer to node
@@ -86,7 +93,7 @@ void Network::addNode(int id, int value)
   }
 } // addNode
 
-void Network::addConnection(int id, int id2)
+void Network::addConnection(int id, int id2, int value)
 {
   Node *node1;
   Node *node2;
@@ -109,15 +116,54 @@ void Network::addConnection(int id, int id2)
 
   if (f1 && f2) // both nodes found and pointers saved
   {
-    node1->connect(id2, node2);
-    node2->connect(id, node1);
+    node1->connect(value, node2);
+    node2->connect(value, node1);
   }
   else // didnt find both nodes
   {
     cout << "addConnection_err: nodes not found" << endl;
   }
 } // addConnection
-// ------------ NETWORK ------------
+
+void Network::traverseNetwork()
+{
+  int currentNodeId = 0; // Start from the root node
+  Node *currentNode = root;
+
+  while (currentNodeId != 999)
+  {
+    // Display current node's ID and connected nodes with their associated values
+    cout << "Current Node: " << currentNodeId << endl;
+    cout << "Connected Nodes:" << endl;
+    for (const auto &pair : currentNode->getConnectedNodes())
+    {
+      cout << "   Node ID: " << pair.second->getId() << ", Value: " << pair.first << endl;
+    }
+    cout << endl;
+
+    // Ask user to choose a connected node or enter 999 to exit
+    cout << "Enter the ID of the node you want to go to (or enter 999 to exit): ";
+    cin >> currentNodeId;
+
+    // Find the next node
+    bool found = false;
+    for (const auto &pair : currentNode->getConnectedNodes())
+    {
+      if (pair.second->getId() == currentNodeId)
+      {
+        currentNode = pair.second;
+        found = true;
+        break;
+      }
+    }
+
+    // If the entered ID is not found among connected nodes, print error message
+    if (!found && currentNodeId != 999)
+    {
+      cout << "Node not found among connected nodes!" << endl;
+    }
+  }
+}
 
 // ------------ MAIN ------------
 int main()
@@ -126,21 +172,18 @@ int main()
   Network network; // creates root node, id: 0
 
   // Add nodes
-  // network.addNode(0, 420);  // id: 1
-  // network.addNode(0, 421);  // id: 2
-  // network.addNode(1, 422);  // id: 3
-  // network.addNode(3, 111);  // id: 4
-  // network.addNode(10, 111); // no node with id 10, wont create node.
+  network.addNode(0, 2);           // id:1
+  network.addNode(0, 6);           // id:2
+  network.addNode(1, 5);           // id:3
+  network.addConnection(3, 2, 8);  // connecting node 2 and 3, with value of 8
+  network.addNode(3, 10);          // id:4
+  network.addNode(4, 6);           // id:5
+  network.addNode(4, 2);           // id:6
+  network.addConnection(3, 5, 15); // connecting node 3 and 5, with value of 15
+  network.addConnection(5, 6, 6);  // connecting node 5 and 6, with value of 6
 
-  network.addNode(0, 2); // id:1
-  network.addNode(0, 6); // id:2
-  network.addNode(1, 5); // id:3
-  // network.addConnection(3, 2); // connecting node 2 and 3
-
-  while (true)
-  {
-  }
+  // Traverse the network
+  network.traverseNetwork();
 
   return 0;
 } // main
-  // ------------ MAIN ------------
