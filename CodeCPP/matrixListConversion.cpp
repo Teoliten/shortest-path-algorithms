dfe454196831d262a01be3b73e83a20f33158f39
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-03-07 09:07:52 +0200

    added print function, converting function returns matrix instead of void

--- CodeCPP/matrixListConversion.cpp
+++ CodeCPP/matrixListConversion.cpp
@@ -3,77 +3,60 @@
 
 using namespace std;
 
+void printMatrix(vector<vector<int>> matrix)
+{
+  cout << "Matrix: " << endl;
+  for (int i = 0; i < matrix.size(); i++)
+  {
+    for (int j = 0; j < matrix.size(); j++)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+} // printMatrix
+
 // void matrixToList()
 // {
-
 // }
 
-void listToMatrix(vector<vector<pair<int, int>>> adjList)
+vector<vector<int>> listToMatrix(const vector<vector<pair<int, int>>> &adjList)
 {
   int nodes = adjList.size();
-  // cout << "Nodes in provided adjacency list: " << nodes << endl;
-
-  int newMatrix[nodes][nodes]; // create new n*n matrix based on nodes in provided adj. list
-  for (int i = 0; i < nodes; i++)
-  {
-    for (int j = 0; j < nodes; j++)
-    {
-      newMatrix[i][j] = 0;
-    }
-  }
+  vector<vector<int>> newMatrix(nodes, vector<int>(nodes, 0)); // create new n*n matrix initialized with 0s
 
   for (int i = 0; i < nodes; i++)
   {
-    for (int j = 0; j < adjList[i].size(); j++) // loop for the amount of edges in node
+    for (int j = 0; j < adjList[i].size(); j++)
     {
       newMatrix[i][adjList[i][j].first] = adjList[i][j].second;
     }
   }
 
-  cout << "MATRIX: " << endl;
-  for (int i = 0; i < nodes; i++)
-  {
-    for (int j = 0; j < nodes; j++)
-    {
-      cout << newMatrix[i][j] << " ";
-    }
-    cout << endl;
-  }
-}
+  return newMatrix;
+} // listToMatrix
 
 int main()
 {
-  // same network of 3 nodes 3 edges represented in both matrix and adjacency list, to check for correct expected output
-  int matrix2[4][4] =
+  // same network of 4 nodes 8 edges represented in both matrix and adjacency list, to check for correct expected output
+  vector<vector<int>> matrix =
       {
           {0, 3, 5, 0},
           {1, 0, 0, 9},
           {0, 4, 0, 0},
-          {4, 2, 2, 0}}; // 3x3 matrix, containing network of 3 nodes and 3 edges with weights.
+          {4, 2, 2, 0}}; // 4x4 matrix, containing network of 4 nodes and 8 edges with weights.
 
-  vector<vector<pair<int, int>>> adjList2 = {
-      // 3 nodes, 5 edges with weights
+  vector<vector<pair<int, int>>> adjList = {
+      // 4 nodes, 8 edges with weights
       {{1, 3}, {2, 5}},          // Node 0: (1,3), (2, 5)
       {{0, 1}, {3, 9}},          // Node 1: (0, 1)
       {{1, 4}},                  // Node 2: (1, 4)
       {{1, 2}, {2, 2}, {0, 4}}}; // Node 3: (1, 2), (2, 2), (0, 4)
 
-  // -----------------------------------------------------------------------------------------
+  // --------------------------------------
 
-  int matrix[3][3] =
-      {
-          {1, 0, 0},
-          {0, 0, 3},
-          {2, 0, 0}}; // 3x3 matrix, containing network of 3 nodes and 3 edges with weights.
-
-  vector<vector<pair<int, int>>> adjList = {
-      // 3 nodes, 5 edges with weights
-      {{1, 4}}, // Node 0: (1,4)
-      {{2, 2}}, // Node 1: (2, 2)
-      {{0, 1}}, // Node 2: (0, 1)
-  };
-
-  listToMatrix(adjList2); // Transform adjacency list to matrix
+  vector<vector<int>> resultMatrix = listToMatrix(adjList); // Transform adjacency list to matrix
+  printMatrix(resultMatrix);                                // print generated matrix
 
   return 0;
 
